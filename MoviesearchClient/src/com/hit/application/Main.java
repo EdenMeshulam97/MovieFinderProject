2e1366e2152fb209aa1995dbb1e0d04df1f5b935
Eden Meshualm <Edenmeshulam97@gmail.com>
2023-07-06 00:50:28 +0300

    client creation

---
+++ MoviesearchClient/src/com/hit/application/Main.java
@@ -0,0 +1,30 @@
+package com.hit.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {			
+			Parent root = (Parent)FXMLLoader.load(getClass().getResource("movieView.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setTitle("Movie Finder");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
